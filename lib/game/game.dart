4be4dea9459f5d4e598b2f81a9a37e346204d072
Jayden1Joe <jojojo69988@naver.com>
2025-06-30 13:41:18 +0900

    Feat(Add data files, add load stats function)

--- lib/game/game.dart
+++ lib/game/game.dart
@@ -1,5 +1,7 @@
 import 'dart:math';
 
+import 'dart:io';
+
 import 'package:battle_rpg_game/models/character.dart';
 import 'package:battle_rpg_game/models/monster.dart';
 
@@ -9,6 +11,51 @@ class Game {
   int monstersDefeated = 0;
 
   Game(this.character, this.monsters);
+
+  void loadCharacterStats() {
+    try {
+      final file = File('data/characters.txt');
+      final contents = file.readAsStringSync();
+      final stats = contents.split(',');
+      if (stats.length != 3) throw FormatException('Invalid character data');
+
+      int health = int.parse(stats[0]);
+      int attack = int.parse(stats[1]);
+      int defense = int.parse(stats[2]);
+
+      String name = getCharacterName();
+      character = Character(name, health, attack, defense);
+    } catch (e) {
+      print('캐릭터 데이터를 불러오는 데 실패했습니다: $e');
+      exit(1);
+    }
+  }
+
+  void loadMonsterStats() {
+    try {
+      final file = File('data/monsters.txt');
+      final lines = file.readAsLinesSync();
+
+      for (var line in lines) {
+        final stats = line.split(',');
+        if (stats.length != 3) {
+          throw FormatException('Invalid monster data: $line');
+        }
+        String name = stats[0];
+        int health = int.parse(stats[1]);
+        int attack = int.parse(stats[2]);
+
+        monsters.add(Monster(name, health, attack));
+      }
+    } catch (e) {
+      print('몬스터 데이터를 불러오는 데 실패했습니다: $e');
+      exit(1);
+    }
+  }
+
+  String getCharacterName() {
+    return '';
+  }
 
   void startGame() {
     // 캐릭터의 체력이 0 이하가 되면 **게임이 종료**됩니다.
